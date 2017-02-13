2ea7f9999b7d627b69cffd8a2fb3858e41cb4aa9
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-02-13 18:52:44 +0100

    -implement sensor_ctl_init service which reads from the sensor the calibration offset, evaluates the arm between the wrist and the com of the tool and the weight of the tool -save offset, arm and weight in controllers.yaml -add yaml-ccp dependency

--- include/lwr_force_position_controllers/ft_sensor_controller.h
+++ include/lwr_force_position_controllers/ft_sensor_controller.h
@@ -1,9 +1,11 @@
-#ifndef LWR_FORCE_POSITION_CONTROLLERS_FT_SENSOR_CONTROLLER_H
-#define LWR_FORCE_POSITION_CONTROLLERS_FT_SENSOR_CONTROLLER_H
+#ifndef LWR_CONTROLLERS_FT_SENSOR_CONTROLLER_H
+#define LWR_CONTROLLERS_FT_SENSOR_CONTROLLER_H
 
 #include <lwr_controllers/KinematicChainControllerBase.h>
 #include <boost/scoped_ptr.hpp>
 #include <geometry_msgs/WrenchStamped.h>
+#include <std_srvs/Empty.h>
+#include <yaml-cpp/yaml.h>
 
 //KDL include
 #include <kdl/chainfksolverpos_recursive.hpp>
@@ -19,14 +21,15 @@ namespace lwr_controllers
     bool init(hardware_interface::JointStateInterface* robot, ros::NodeHandle& nh);
     void starting(const ros::Time& time);
     void update(const ros::Time& time, const ros::Duration& period);
-
     
   private:
     void ft_sensor_topic_callback(const geometry_msgs::WrenchStamped::ConstPtr& msg);
-    void set_sensor_offset(std::vector<double>& wrench);
-    void set_p_wrist_toolcom(std::vector<double>& p);
-    void set_base_tool_weight_com(double mass);
     void publish_data(KDL::Wrench wrench, ros::Publisher& pub);
+    bool set_sensor_initial_conditions(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res);
+    void write_vector_to_yaml(std::string field, KDL::Wrench wrench);
+    void write_vector_to_yaml(std::string field, KDL::Vector vector);
+    void set_wrench(std::vector<double>& value, KDL::Wrench& wrench);
+    void set_vector(std::vector<double>& p, KDL::Vector& vector);
 
     ros::Time last_publish_time_;
     boost::scoped_ptr<KDL::ChainFkSolverPos_recursive> fk_solver_;
@@ -35,6 +38,9 @@ namespace lwr_controllers
     double publish_rate_;
     KDL::Wrench ft_sensor_offset_, ft_wrench_raw_, base_tool_weight_com_;
     KDL::Vector p_wrist_toolcom_;
+    
+    // set_sensor_initial_conditions service
+    ros::ServiceServer sensor_ctl_init_service_;
 
   };
 
--- src/ft_sensor_controller.cpp
+++ src/ft_sensor_controller.cpp
@@ -1,8 +1,11 @@
 #include <pluginlib/class_list_macros.h>
 #include <lwr_force_position_controllers/ft_sensor_controller.h>
 #include <kdl_conversions/kdl_msg.h>
+#include <eigen_conversions/eigen_kdl.h>
 #include <kdl/frames_io.hpp>
 #include <math.h>
+#include <utils/skew_pinv.h>
+#include <ros/package.h>
 
 namespace lwr_controllers {
 
@@ -12,7 +15,7 @@ namespace lwr_controllers {
   bool FtSensorController::init(hardware_interface::JointStateInterface *robot, ros::NodeHandle& nh)
   {
     KinematicChainControllerBase<hardware_interface::JointStateInterface>::init(robot, nh);
-
+    
     // instantiate forward kinematic solver
     fk_solver_.reset(new KDL::ChainFkSolverPos_recursive(kdl_chain_));
     
@@ -29,23 +32,27 @@ namespace lwr_controllers {
     nh.getParam("publish_rate", publish_rate_);
 
     // get the ft sensor offset from rosparam and set ft_sensor_offset_
-    std::vector<double> wrench;
-    nh.getParam("ft_sensor_offset", wrench);
-    set_sensor_offset(wrench);
+    std::vector<double> ft_sensor_offset;
+    nh.getParam("ft_sensor_offset", ft_sensor_offset);
+    set_wrench(ft_sensor_offset, ft_sensor_offset_);
 
     // get p_wrist_toolcom and set p_wrist_toolcom_
     // p_wrist_toolcom_ = vector from wrist center to the com of the tool
     std::vector<double> p;
     nh.getParam("p_wrist_toolcom", p);
-    set_p_wrist_toolcom(p);
+    set_vector(p, p_wrist_toolcom_);
 
     // get tool mass and set base_tool_weight_com_
     // base_tool_weight_com_ = weight due to tool mass with reference point on its com
     // projected in world frame
-    double mass;
-    nh.getParam("tool_mass", mass);
-    set_base_tool_weight_com(mass);
+    std::vector<double> weight;
+    nh.getParam("base_tool_weight_com", weight);
+    set_wrench(weight, base_tool_weight_com_);
 
+    // advertise SensorCtlInitService service
+    sensor_ctl_init_service_ = nh.advertiseService("sensor_ctl_init",\
+						   &FtSensorController::set_sensor_initial_conditions,\
+						   this);
     return true;
   }
 
@@ -74,15 +81,15 @@ namespace lwr_controllers {
     KDL::Frame fk_frame;
     fk_solver_->JntToCart(joint_position, fk_frame);
 
-    // transform wrench from end effector frame to world frame
-    wrench = fk_frame.M * wrench;
-
     // compesante for tool weight
     // move the reference point of the weight from the com of the tool to the wrist
     KDL::Frame gravity_transformation(KDL::Rotation::Identity(),\
 				      fk_frame.M * p_wrist_toolcom_);
     // compensate for the weight of the tool
-    wrench_nog = wrench - gravity_transformation * base_tool_weight_com_;
+    wrench_nog = fk_frame.M * wrench - gravity_transformation * base_tool_weight_com_;
+
+    // the real would output measurements in end-effector frame
+    wrench_nog = fk_frame.M.Inverse() * wrench_nog;
 
     if(time > last_publish_time_ + ros::Duration(1.0 / publish_rate_))
       {
@@ -115,25 +122,87 @@ namespace lwr_controllers {
     tf::wrenchMsgToKDL(msg->wrench, ft_wrench_raw_);
   }
 
-  void FtSensorController::set_sensor_offset(std::vector<double>& wrench)
+  bool FtSensorController::set_sensor_initial_conditions(std_srvs::Empty::Request& req,	\
+							 std_srvs::Empty::Response& res)
   {
     // set ft_sensor_offset_
-    ft_sensor_offset_ = KDL::Wrench(KDL::Vector(wrench.at(0), wrench.at(1), wrench.at(2)), \
-				    KDL::Vector(wrench.at(3), wrench.at(4), wrench.at(5)));
+    ft_sensor_offset_ = ft_wrench_raw_;
+
+    // evaluate end-effector weight
+    base_tool_weight_com_ = KDL::Wrench(KDL::Vector(0, 0, -ft_sensor_offset_.force.Norm()),\
+					KDL::Vector::Zero());
+    
+    // evaluate the coordinates of the tool com
+    // torque = p_wrist_toolcom x force = -force x p_wrist_toolcom
+    //        = -skew(force) * p_wrist_toolcom
+
+    // p_wrist_toolcom = - pinv(skew(force)) * torque
+    Eigen::Matrix<double, 3, 1> force;
+    Eigen::Matrix<double, 3, 1> torque;
+    Eigen::Matrix3d pinv;
+    
+    tf::vectorKDLToEigen(ft_sensor_offset_.force, force);
+    tf::vectorKDLToEigen(ft_sensor_offset_.torque, torque);
+
+    skew_pinv(force, pinv);
+    tf::vectorEigenToKDL(-pinv * torque, p_wrist_toolcom_);
+
+    // save to file    
+    write_vector_to_yaml("ft_sensor_offset", ft_sensor_offset_);
+    write_vector_to_yaml("base_tool_weight_com", base_tool_weight_com_);
+    write_vector_to_yaml("p_wrist_toolcom", p_wrist_toolcom_);
+
+    return true;
   }
 
-  void FtSensorController::set_p_wrist_toolcom(std::vector<double>& p)
+  void FtSensorController::write_vector_to_yaml(std::string field, KDL::Wrench wrench)
   {
-    // set p_wrist_toolcom_
-    p_wrist_toolcom_ = KDL::Vector(p.at(0), p.at(1), p.at(2));			
+    // save vector to controllers.yaml
+    std::string file_name = ros::package::getPath("lwr_force_position_controllers") + "/config/controllers.yaml";
+
+    YAML::Node controllers_yaml = YAML::LoadFile(file_name);
+
+    std::vector<double> value(6);
+    value.at(0) = wrench.force.x();
+    value.at(1) = wrench.force.y();
+    value.at(2) = wrench.force.z();
+    value.at(3) = wrench.torque.x();
+    value.at(4) = wrench.torque.y();
+    value.at(5) = wrench.torque.z();
+
+    controllers_yaml["lwr"]["ft_sensor_controller"][field] = value;
+    std::ofstream fout(file_name); 
+    fout << controllers_yaml;
   }
 
-  void FtSensorController::set_base_tool_weight_com(double mass)
+  void FtSensorController::write_vector_to_yaml(std::string field, KDL::Vector vector)
   {
-    // gravity_ defined in KinematicChainControllerBase
-    base_tool_weight_com_ = KDL::Wrench(mass * gravity_, KDL::Vector::Zero());
+    // save vector to controllers.yaml
+    std::string file_name = ros::package::getPath("lwr_force_position_controllers") + "/config/controllers.yaml";
+    
+    YAML::Node controllers_yaml = YAML::LoadFile(file_name);
+
+    std::vector<double> value(3);
+    value.at(0) = vector.x();
+    value.at(1) = vector.y();
+    value.at(2) = vector.z();
+
+    controllers_yaml["lwr"]["ft_sensor_controller"][field] = value;
+    std::ofstream fout(file_name); 
+    fout << controllers_yaml;
+  }
+
+  void FtSensorController::set_wrench(std::vector<double>& value, KDL::Wrench& wrench)
+  {
+    wrench = KDL::Wrench(KDL::Vector(value.at(0), value.at(1), value.at(2)), \
+			 KDL::Vector(value.at(3), value.at(4), value.at(5)));
+  }
+
+  void FtSensorController::set_vector(std::vector<double>& p, KDL::Vector& vector)
+  {
+    vector = KDL::Vector(p.at(0), p.at(1), p.at(2));
   }
     
 } // namespace
 
-PLUGINLIB_EXPORT_CLASS(lwr_controllers::FtSensorController , controller_interface::ControllerBase)
+PLUGINLIB_EXPORT_CLASS(lwr_controllers::FtSensorController, controller_interface::ControllerBase)
